bf5fc630349381a117cb8eeaa8572d4d5fe8f69e
Yehudis Schnaidman <yehudis111@gmail.com>
2026-01-25 23:23:07 -0500

    add class SalesRep under Employee @yehudis

---
+++ BellaAndYehudisProject/Entities/SalesRep.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    ///add inheriter sales rep to employee class
+    internal class SalesRep
+        :Employee
+    {
+        public int CommissionRate { get; set; } //what is the sales reps comission rate?
+
+        public SalesRep(string id, string name, double salary, int commisionrate)
+            :base(name, id, salary)
+        {
+            CommissionRate = commisionrate;  
+        }
+    }
+}
