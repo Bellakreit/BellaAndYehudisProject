ae5b441a76f56cc68e785f8071fbd9254ae3e855
Yehudis Schnaidman <yehudis111@gmail.com>
2026-01-25 23:19:44 -0500

    add Class Manager under Employee @yehudis

---
+++ BellaAndYehudisProject/Entities/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    //adds class Manager under the employee class
+    internal class Manager
+        :Employee
+    {
+        public int Vetek { get; set; }  // i think how many years worked??? unsure 
+        /// <summary>
+        /// takes objects for manager and assigns them in a ctor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="salary"></param>
+        /// <param name="vetek"></param>
+        public Manager(string id, string name, double salary, int vetek)
+            :base(name, id, salary)
+        {
+            Vetek = vetek;
+        }
+    }
+}
