c9487f99012326ea2267f9923fb40f50fa373795
Bellakreit <bella@kreit.net>
2026-02-25 23:12:26 -0800

    adding project BL and crud methods

---
+++ BellaAndYehudisProject/BL/ProductBL.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using DAL;
+
+namespace BL
+{
+    public class ProductBL
+    {
+        ProductDAL pd1 = new ProductDAL();  //instance variable of type productDAL
+
+        public ProductBL()   //ctor 
+        {
+            pd1 = new ProductDAL();
+        }
+
+        public void CreateProduct(Product temp) //create product
+        {
+            try
+            {
+                pd1.Create(temp);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public List<Product> Read() //read all products
+        {
+            try
+            {
+                return pd1.Read();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public Product Read(int id) //read one product
+        {
+            try
+            {
+                return pd1.Read(id);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public void UpdateProduct(Product temp)  //update product
+        {
+            try
+            {
+                pd1.Update(temp);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void DeleteProduct(Product temp)    //delete product
+        {
+            try
+            {
+                pd1.Delete(temp);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
